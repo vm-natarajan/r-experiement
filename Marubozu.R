bedf0fddb83f6445d7a5a5285957d4a59589a6b0
Vm Natarajan <veera@Manikandans-MacBook-Air.local>
2018-08-12 19:52:00 -0500

    Including function to find patterns for Bearish

--- Marubozu.R
+++ Marubozu.R
@@ -1,4 +1,8 @@
-findBullishMarubozu <- function (dataset,body=40,ls=30,us=40){
+
+#1.1 To find occurence of bullish Marubozu by passed dataset,along with lengths of body,lowershadow and upper
+#shadow
+
+findBullishMarubozu <- function (dataset,body=80,ls=25,us=25){
   
   #if(dataset$Close.Price>dataset$Open.Price)
   
@@ -28,6 +32,42 @@ findBullishMarubozu <- function (dataset,body=40,ls=30,us=40){
     dataset <- dataset[i_result$l_body>=body & i_result$l_lowershadow<=ls & i_result$l_uppershadow<=us,];
     
     return(dataset);
-
 }    
+
+
+#1.2 To find occurence of Bearish Marubozu by passed dataset,along with lengths of body,lowershadow and upper
+#shadow
+
+findBearishMarubozu <- function (dataset,body=80,ls=25,us=25){
+  
+  #if(dataset$Close.Price>dataset$Open.Price)
+  
+  dataset <- dataset[dataset$Close.Price<dataset$Open.Price,]
+  
+  #calculate body length of the candle stick
+  candlestickbody <- dataset$Open.Price - dataset$Close.Price;
+  
+  #calculate lower shadow of the candle stick
+  lowershodow <- dataset$Close.Price - dataset$Low.Price
+  
+  #calculate upper shodow of the candle stick
+  uppershadow <- dataset$High.Price - dataset$Open.Price;
+  
+  #set the sum of above values as benchmark
+  benchmark <- candlestickbody + lowershodow + uppershadow;
+  
+  #Calculate the percentage of parts against benchmark
+  l_body <- round((candlestickbody/benchmark),digits = 3)*100;
+  l_lowershadow <- round((lowershodow/benchmark),digits = 3)*100;
+  l_uppershadow <- round((uppershadow/benchmark),digits = 3)*100;
+  
+  #Dataframe to keep hold of part elements in percentage
+  i_result <- as.data.frame(cbind(l_body,l_lowershadow,l_uppershadow));
+  
+  #The final dataframe that returns bullish data by applying the given filter parameters
+  dataset <- dataset[i_result$l_body>=body & i_result$l_lowershadow<=ls & i_result$l_uppershadow<=us,];
+  
+  return(dataset);
+}    
+
 
