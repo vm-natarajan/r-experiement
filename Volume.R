53f21d1cb0d490d2346dabcf38d230e2e75d6047
Vm Natarajan <veera@Manikandans-MacBook-Air.local>
2018-08-18 00:19:13 -0500

    Volume algorithm updated

--- Volume.R
+++ Volume.R
@@ -1,20 +1,30 @@
-findPatternUsingVolume <- function(dataset,days = 4){
+findPatternUsingVolume <- function(dataset,days = 5){
+  
+  volume <- data.frame(matrix(ncol = 3, nrow = 0))
+  col_names <- c("security", "date", "trend")
+  colnames(volume) <- col_names
   for(row in 1:nrow(dataset)){
-    if(row < days){
+    if(row <= (nrow(dataset)-days)){
+
       previous_days_dataset <- dataset[c((row+1):(row+days)),];
-      volume_average <- mean(previous_days_dataset$No.of.Shares);
       wap_average <- mean(previous_days_dataset$WAP);
-      #,"WAP"
-      #,"No.of.Shares"
-      print(dataset$No.of.Shares[row]);
-      print(volume_average);
-      print(dataset$WAP[row]);
-      print(wap_average);
-      print((dataset$No.of.Shares[row] > volume_average) && (dataset$WAP[row] > wap_average));
-      if(dataset$No.of.Shares[row] > volume_average && dataset$WAP[row] > wap_average){
-        result <- as.data.frame(result,cbind(dataset[row,]))
-        print(result);
+      volume_average <- mean(previous_days_dataset$No.of.Shares);
+      
+      if((dataset$WAP[row] > wap_average) && (dataset$No.of.Shares[row] > volume_average)){
+        new_row <- data.frame(security = dataset$Security[row],date = dataset$Date[row],trend="II");
+      }else if((dataset$WAP[row] < wap_average) && (dataset$No.of.Shares[row] > volume_average)){
+        new_row <- data.frame(security = dataset$Security[row],date = dataset$Date[row],trend="DI");
+      }else if((dataset$WAP[row] < wap_average) && (dataset$No.of.Shares[row] < volume_average)){
+        new_row <- data.frame(security = dataset$Security[row],date = dataset$Date[row],trend="DD");
+      }else if ((dataset$WAP[row] > wap_average) && (dataset$No.of.Shares[row] < volume_average)){
+        new_row <- data.frame(security = dataset$Security[row],date = dataset$Date[row],trend="ID");
+      }else{
+        new_row <- data.frame(security = dataset$Security[row],date = dataset$Date[row],trend="NC");
       }
+      
+      volume <- rbind(volume,new_row);
     }
   }
+  return(volume);
+  
 }
