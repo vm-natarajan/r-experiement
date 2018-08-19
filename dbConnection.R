9a9d9eede4c94833f162b40afbf9cd612fad91f4
Vm Natarajan <veera@Manikandans-MacBook-Air.local>
2018-08-19 15:04:44 -0500

    Re-org

--- dbConnection.R
+++
@@ -1,6 +0,0 @@
-db <- function(tableName){
-  library(RMySQL);
-  db = dbConnect(MySQL(), user='root', password='Testtest1', dbname='bse', host='localhost');
-  rs <- dbGetQuery(db,paste("select * from",tableName ));
-  return(rs);
-}
